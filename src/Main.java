62733e1bfb7f235d6b9b03decbb3e8125dbc8ba8
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-03-18 20:44:26 +0100

    innit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,91 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+/* Operaciones a realizar:
+    - Crear una cuenta y para ello solicitar el DNI y titular, el saldo se inicializa en 0.
+    - Sacar dinero, para ello debes comprobar que hay saldo.
+    - Ingresar dinero, sumarle al saldo el nuevo importe insertado.
+
+* Sobre cargar los métodos constructores, para poder crear objetos a partir del dni, sin nombre y saldo.
+* Más otro método constructor con dni, titular y saldo.
+
+* Siguiente cambio: Agregar un atributo estático del nombreBanco...
+*/
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        menu();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    static void menu(){
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
+
+        //Creamos el objeto
+        CuentaCorriente cta=new CuentaCorriente();
+        do {
+            System.out.println("|********* Menú "+ CuentaCorriente.nombreBanco +" ********|");
+            System.out.println("|-----------------------------------|");
+            System.out.println("|**** 1. Crear Cuenta sin Saldo ****|");
+            System.out.println("|**** 2. Crear Cuenta con Saldo ****|");
+            System.out.println("|**** 3. Ingresar Saldo         ****|");
+            System.out.println("|**** 4. Retirar Saldo          ****|");
+            System.out.println("|**** 5. Mostrar Datos          ****|");
+            System.out.println("|**** 6. Salir                  ****|");
+            System.out.println("|___________________________________|");
+            try {
+                opcion = sc.nextInt();
+                sc.nextLine();
+                switch (opcion) {
+                    case 1 ->{
+                        System.out.println("Ingrese su nombre: ");
+                        cta.setTitular(sc.nextLine());
+                        System.out.println("Ingrese su DNI: ");
+                        cta.setDni(sc.next());
+                        cta.getDni();
+                    }
+                    case 2 ->{
+                        System.out.println("Ingrese su nombre: ");
+                        cta.setTitular(sc.nextLine());
+                        System.out.println("Ingrese su DNI: ");
+                        cta.setDni(sc.next());
+                        System.out.println("Indique el saldo a ingresar: ");
+                        try {
+                            cta.setSaldo(sc.nextDouble());
+                        }catch (Exception e){
+                            System.out.println("El dato ingresado no es correcto");
+                        }
+                    }
+                    case 3 ->{
+                        System.out.println("Indique el saldo a ingresar: ");
+                        try {
+                            cta.setSaldo(sc.nextDouble());
+                        }catch (Exception e){
+                            System.out.println("El dato ingresado no es correcto");
+                        }
+                    }
+                    case 4 ->{
+                        System.out.println("Indique el saldo a retirar: ");
+                        try {
+                            cta.retirarSaldo(sc.nextDouble());
+                        }catch (Exception e){
+                            System.out.println("El dato ingresado es incorrecto");
+                        }
+                    }
+                    case 5 ->{
+                        System.out.println(cta.toString());
+                    }
+                    case 6 ->{
+                        System.out.println("Saliendo.... ¡Hasta pronto!");
+                    }
+                    default ->{
+                        System.out.println("Opción no válida");
+                    }
+                }
+            }catch (Exception e){
+                System.out.println("Opción no válida");
+                //limpiar el buffer
+                sc.next();
+            }
+        }while(opcion!=6);
     }
 }
